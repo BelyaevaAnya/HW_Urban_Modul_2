c1fc613c0b44525adad67175b0889d920593a112
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-24 19:22:51 +0300

    HW_5

--- module_2_5.py
+++ module_2_5.py
@@ -12,6 +12,13 @@ def get_matrix(n=3, m=3, value=1):
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+result4 = get_matrix(0, 0, 0)
+result5 = get_matrix(5, 0, 0)
+result6 = get_matrix(0, 5, 0)
+print(result1)  # => [[10, 10], [10, 10]]
+print(result2)  # => [[42, 42, 42, 42, 42], [42, 42, 42, 42, 42], [42, 42, 42, 42, 42]]
+print(result3)  # => [[13, 13], [13, 13], [13, 13], [13, 13]]
+print(result4)  # => []
+print(result5)  # => [[], [], [], [], []]
+print(result6)  # => []
+
